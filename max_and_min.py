f06fa89d88968b2ed27afc2ffb4218bfa3c4e634
RAJALEKSHMI-R <115915325+Rajalekshmir1331@users.noreply.github.com>
2023-10-20 03:03:54 -0700

    Add files via upload

---
+++ max_and_min.py
@@ -0,0 +1,8 @@
+import math
+a=int(input("enter the first number"));
+b=int(input("enter the second number"));
+c=int(input("enter the third number"));
+minimum=min(a,b,c)
+maximum=max(a,b,c)
+print("the smallest among the given number is=",minimum);
+print("the largest among the number is=",maximum);
